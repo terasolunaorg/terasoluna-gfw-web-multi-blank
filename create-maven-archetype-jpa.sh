ea8ea5c69d2b766c996f4a4c477321c3d5ad1907
Toshiaki Maki <makingx@gmail.com>
2015-01-27 11:43:17 +0900

    changed description #101

--- create-maven-archetype-jpa.sh
+++ create-maven-archetype-jpa.sh
@@ -2,5 +2,5 @@
 sh change-infra.sh JPA
 sed -i -e "s/terasoluna-gfw-multi-web-blank/terasoluna-gfw-multi-web-blank-jpa/g" pom.xml
 sed -i -e "s/terasoluna-gfw-multi-web-blank/terasoluna-gfw-multi-web-blank-jpa/g" create-maven-archetype.sh
-sed -i -e "s/TERASOLUNA Web Blank Multi Project/TERASOLUNA Web Blank Multi Project (JPA)/g" pom.xml
+sed -i -e "s/Web Blank Multi Project/Web Blank Multi Project (JPA)/g" pom.xml
 sh create-maven-archetype.sh
--- create-maven-archetype-mybatis2.sh
+++ create-maven-archetype-mybatis2.sh
@@ -2,5 +2,5 @@
 sh change-infra.sh MyBatis2
 sed -i -e "s/terasoluna-gfw-multi-web-blank/terasoluna-gfw-multi-web-blank-mybatis2/g" pom.xml
 sed -i -e "s/terasoluna-gfw-multi-web-blank/terasoluna-gfw-multi-web-blank-mybatis2/g" create-maven-archetype.sh
-sed -i -e "s/TERASOLUNA Web Blank Multi Project/TERASOLUNA Web Blank Multi Project (MyBatis2)/g" pom.xml
+sed -i -e "s/Web Blank Multi Project/Web Blank Multi Project (MyBatis2)/g" pom.xml
 sh create-maven-archetype.sh
