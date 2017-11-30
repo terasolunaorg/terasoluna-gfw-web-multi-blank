7c196be1d78950cda0525ae602c75c01e2cb01c6
btyanagawamg <yanagawamg@pm.nttdata.co.jp>
2017-11-30 17:56:38 +0900

    be able to select install in addition to deploy #368
    (cherry picked from commit 9aa2c18838b0b9ef264541425fb23911bce253a4)

--- create-maven-archetype-jpa.sh
+++ create-maven-archetype-jpa.sh
@@ -3,4 +3,4 @@ sh change-infra.sh JPA
 sed -i -e "s/terasoluna-gfw-multi-web-blank/terasoluna-gfw-multi-web-blank-jpa/g" pom.xml
 sed -i -e "s/terasoluna-gfw-multi-web-blank/terasoluna-gfw-multi-web-blank-jpa/g" create-maven-archetype.sh
 sed -i -e "s/Web Blank Multi Project/Web Blank Multi Project (JPA)/g" pom.xml
-sh create-maven-archetype.sh "$1"
+sh create-maven-archetype.sh "$1" "$2"
