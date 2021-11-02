78742db4d615ddd63d79db3439226b7fc981fe07
btonokit <Keita.Ono.bp@nttdata.com>
2021-11-02 16:27:24 +0900

    replaced RequestMapping with GetMapping #550

--- projectName-web/src/main/java/xxxxxx/yyyyyy/zzzzzz/app/welcome/HelloController.java
+++ projectName-web/src/main/java/xxxxxx/yyyyyy/zzzzzz/app/welcome/HelloController.java
@@ -8,8 +8,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 
 /**
  * Handles requests for the application home page.
@@ -23,7 +22,7 @@ public class HelloController {
     /**
      * Simply selects the home view to render by returning its name.
      */
-    @RequestMapping(value = "/", method = {RequestMethod.GET, RequestMethod.POST})
+    @GetMapping(value = "/")
     public String home(Locale locale, Model model) {
         logger.info("Welcome home! The client locale is {}.", locale);
 
