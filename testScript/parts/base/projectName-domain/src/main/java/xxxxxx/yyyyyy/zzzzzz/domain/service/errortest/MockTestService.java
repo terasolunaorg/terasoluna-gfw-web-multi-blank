068921f68188453c62bd9b788472deb8c63c9146
btsekinedaisuke <130430538+btsekinedaisuke@users.noreply.github.com>
2023-07-20 16:02:52 +0900

    Add internal test #625 #TSF4J5-2803 (#626)

---
+++ testScript/parts/base/projectName-domain/src/main/java/xxxxxx/yyyyyy/zzzzzz/domain/service/errortest/MockTestService.java
@@ -0,0 +1,6 @@
+package xxxxxx.yyyyyy.zzzzzz.domain.service.errortest;
+
+/** Services for mock testing */
+public interface MockTestService {
+    void testExecute();
+}
---
+++ testScript/parts/base/projectName-web/src/main/java/xxxxxx/yyyyyy/zzzzzz/app/errortest/MockTestController.java
@@ -0,0 +1,27 @@
+package xxxxxx.yyyyyy.zzzzzz.app.errortest;
+
+import javax.inject.Inject;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import xxxxxx.yyyyyy.zzzzzz.domain.service.errortest.MockTestService;
+
+/**
+ * Controller for Mock test
+ */
+@Controller
+@RequestMapping("/test/mock")
+public class MockTestController {
+
+    @Inject
+    MockTestService mockTestService;
+
+    @GetMapping(value = "/success")
+    public String test(Model model) {
+        mockTestService.testExecute();
+        return "welcome/home";
+    }
+}
