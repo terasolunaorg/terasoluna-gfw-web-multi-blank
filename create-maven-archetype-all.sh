a1e3e4dc17b182054d2aa95b0b82585741ed21c8
btootomoyuk <57735558+btootomoyuk@users.noreply.github.com>
2023-11-27 13:33:56 +0900

    Modified shell scripts to add javaconfig version #635 (#636)
    ---
    TSF4J5-3179

--- create-maven-archetype-all.sh
+++ create-maven-archetype-all.sh
@@ -4,11 +4,11 @@ set -e
 DEPLOY=$1
 REPOSITORY=$2
 
-#sh create-maven-archetype-javaconfig-jsp-jpa.sh "$DEPLOY" "$REPOSITORY"
-#sh create-maven-archetype-javaconfig-jsp-mybatis3.sh "$DEPLOY" "$REPOSITORY"
-#sh create-maven-archetype-javaconfig-thymeleaf-jpa.sh "$DEPLOY" "$REPOSITORY"
-#sh create-maven-archetype-javaconfig-thymeleaf-mybatis3.sh "$DEPLOY" "$REPOSITORY"
+sh create-maven-archetype-javaconfig-jsp-jpa.sh "$DEPLOY" "$REPOSITORY"
+sh create-maven-archetype-javaconfig-jsp-mybatis3.sh "$DEPLOY" "$REPOSITORY"
+sh create-maven-archetype-javaconfig-thymeleaf-jpa.sh "$DEPLOY" "$REPOSITORY"
+sh create-maven-archetype-javaconfig-thymeleaf-mybatis3.sh "$DEPLOY" "$REPOSITORY"
 sh create-maven-archetype-xmlconfig-jsp-jpa.sh "$DEPLOY" "$REPOSITORY"
 sh create-maven-archetype-xmlconfig-jsp-mybatis3.sh "$DEPLOY" "$REPOSITORY"
-#sh create-maven-archetype-xmlconfig-thymeleaf-jpa.sh "$DEPLOY" "$REPOSITORY"
-#sh create-maven-archetype-xmlconfig-thymeleaf-mybatis3.sh "$DEPLOY" "$REPOSITORY"
+sh create-maven-archetype-xmlconfig-thymeleaf-jpa.sh "$DEPLOY" "$REPOSITORY"
+sh create-maven-archetype-xmlconfig-thymeleaf-mybatis3.sh "$DEPLOY" "$REPOSITORY"
