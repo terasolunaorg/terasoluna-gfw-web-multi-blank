acc50b6a1e5d1d6b664c769649883c0f84ff4431
btootomoyuk <57735558+btootomoyuk@users.noreply.github.com>
2023-12-28 17:18:59 +0900

    add webdriver settings #641 (#643)
    add webdriver settings #641
    ---
    TSF4J5-3821

---
+++ testScript/parts/base/projectName-selenium/src/test/java/xxxxxx/yyyyyy/zzzzzz/selenium/welcome/HelloIT.java
@@ -0,0 +1,32 @@
+package xxxxxx.yyyyyy.zzzzzz.selenium.welcome;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import java.io.IOException;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.By;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import xxxxxx.yyyyyy.zzzzzz.selenium.FunctionTestSupport;
+
+/**
+ * Executes the test for the application home page.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath:META-INF/spring/seleniumContext.xml" })
+public class HelloIT extends FunctionTestSupport{
+
+    /**
+     * Asserts that the content of the application home page is "Hello world!".
+     */
+    @Test
+    public void testHelloWorld() throws IOException {
+
+        assertThat(webDriver.findElement(By.id("title")).getText(),
+                is("Hello world!"));
+    }
+}
