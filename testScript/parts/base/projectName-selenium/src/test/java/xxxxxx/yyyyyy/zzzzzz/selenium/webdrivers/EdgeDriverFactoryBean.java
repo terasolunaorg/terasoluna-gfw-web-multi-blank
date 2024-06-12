c6544b7ccf22ab95bb25b2b07500185125ac7aba
btkatoufm <Fumio.Kato.bp@nttdata.com>
2024-06-12 13:11:09 +0900

    Fixed selenium to be implemented headless. In addition, the process was reviewed and standardized. #685

--- testScript/parts/base/projectName-selenium/src/test/java/xxxxxx/yyyyyy/zzzzzz/selenium/webdrivers/EdgeDriverFactoryBean.java
+++ testScript/parts/base/projectName-selenium/src/test/java/xxxxxx/yyyyyy/zzzzzz/selenium/webdrivers/EdgeDriverFactoryBean.java
@@ -6,26 +6,22 @@ import org.openqa.selenium.edge.EdgeOptions;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class EdgeDriverFactoryBean extends
-                                   WebDriverManagerFactoryBean<EdgeDriver> {
+                                   HeadlessWebDriverManagerFactoryBean<EdgeDriver> {
 
     @Override
-    public EdgeDriver getObject() {
-        if (System.getenv("webdriver.edge.driver") == null) {
-            WebDriverManager.edgedriver().setup();
-        }
+    protected WebDriverManager getWebDriverManager() {
+        return WebDriverManager.edgedriver();
+    }
+
+    @Override
+    protected EdgeDriver createWebDriver() {
+
         EdgeOptions options = new EdgeOptions();
-        options.addArguments("--remote-allow-origins=*");
+
+        if (super.headless) {
+            options.addArguments("--headless=new");
+        }
+
         return new EdgeDriver(options);
     }
-
-    @Override
-    public Class<?> getObjectType() {
-        return EdgeDriver.class;
-    }
-
-    @Override
-    public boolean isSingleton() {
-        return false;
-    }
-
 }
--- testScript/parts/base/projectName-selenium/src/test/java/xxxxxx/yyyyyy/zzzzzz/selenium/webdrivers/WebDriverManagerFactoryBean.java
+++ testScript/parts/base/projectName-selenium/src/test/java/xxxxxx/yyyyyy/zzzzzz/selenium/webdrivers/WebDriverManagerFactoryBean.java
@@ -2,26 +2,43 @@ package xxxxxx.yyyyyy.zzzzzz.selenium.webdrivers;
 
 import org.openqa.selenium.WebDriver;
 import org.springframework.beans.factory.FactoryBean;
-import org.springframework.beans.factory.InitializingBean;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public abstract class WebDriverManagerFactoryBean<T extends WebDriver>
-                                                 implements FactoryBean<T>,
-                                                 InitializingBean {
+                                                 implements FactoryBean<T> {
 
-    private String propertyFileLocation;
+    protected String propertyFileLocation;
 
     public void setPropertyFileLocation(String propertyFileLocation) {
         this.propertyFileLocation = propertyFileLocation;
     }
 
+    protected abstract WebDriverManager getWebDriverManager();
+
+    protected abstract T createWebDriver();
+
     @Override
-    public void afterPropertiesSet() throws Exception {
-        if (propertyFileLocation != null) {
-            WebDriverManager.firefoxdriver().config().setProperties(
-                    propertyFileLocation);
+    public T getObject() {
+        if (System.getenv("webdriver.driver") == null) {
+            WebDriverManager manager = getWebDriverManager();
+            if (this.propertyFileLocation != null) {
+                manager.config().setProperties(this.propertyFileLocation);
+            }
+            manager.setup();
         }
+
+        return createWebDriver();
+    }
+
+    @Override
+    public Class<?> getObjectType() {
+        return WebDriver.class;
+    }
+
+    @Override
+    public boolean isSingleton() {
+        return false;
     }
 
 }
