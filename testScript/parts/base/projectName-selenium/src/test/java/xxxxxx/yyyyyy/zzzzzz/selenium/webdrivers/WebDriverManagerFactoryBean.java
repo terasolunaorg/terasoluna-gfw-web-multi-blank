ceaa2f414e0a70414e22cc7a01faa89358f4f2ef
btkatoufm <66342857+btkatoufm@users.noreply.github.com>
2024-06-21 13:35:02 +0900

    [5.7.x] Make it headless compatible. #687 (#688)
    * The process around DRIVER was reviewed. Also, added headless option. #687
    
    * Fixed to JDK8 writing style. #687

---
+++ testScript/parts/base/projectName-selenium/src/test/java/xxxxxx/yyyyyy/zzzzzz/selenium/webdrivers/WebDriverManagerFactoryBean.java
@@ -0,0 +1,44 @@
+package xxxxxx.yyyyyy.zzzzzz.selenium.webdrivers;
+
+import org.openqa.selenium.WebDriver;
+import org.springframework.beans.factory.FactoryBean;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public abstract class WebDriverManagerFactoryBean<T extends WebDriver>
+                                                 implements FactoryBean<T> {
+
+    protected String propertyFileLocation;
+
+    public void setPropertyFileLocation(String propertyFileLocation) {
+        this.propertyFileLocation = propertyFileLocation;
+    }
+
+    protected abstract WebDriverManager getWebDriverManager();
+
+    protected abstract T createWebDriver();
+
+    @Override
+    public T getObject() {
+        if (System.getenv("webdriver.driver") == null) {
+            WebDriverManager manager = getWebDriverManager();
+            if (this.propertyFileLocation != null) {
+                manager.config().setProperties(this.propertyFileLocation);
+            }
+            manager.setup();
+        }
+
+        return createWebDriver();
+    }
+
+    @Override
+    public Class<?> getObjectType() {
+        return WebDriver.class;
+    }
+
+    @Override
+    public boolean isSingleton() {
+        return false;
+    }
+
+}
